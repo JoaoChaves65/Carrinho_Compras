12b88ff98f879a08e0e34bf2783a5ba52609ebb9
JoaoChaves65 <joaomarcosgol2000@gmail.com>
2023-05-13 18:38:05 -0300

    CSS do component SearchBar

--- src/components/SearchBar/SearchBar.jsx
+++ src/components/SearchBar/SearchBar.jsx
@@ -1,17 +1,19 @@
 import React from 'react';
 import { BsSearch } from 'react-icons/bs';
 
+import './SearchBar.css';
+
 function SearchBar() {
   return (
-    <form>
+    <form className="search-bar">
       <input
         type="search"
         placeholder="Buscar produtos"
-        className="search__input"
+        className="search_input"
         required
       />
 
-      <button type="submit" className="search__button">
+      <button type="submit" className="search_button">
         <BsSearch />
       </button>
     </form>
