1b2748e7f66ddca651153e0e3aa0b92850cf0cf6
JoaoChaves65 <joaomarcosgol2000@gmail.com>
2023-05-15 16:16:00 -0300

    Usando o useState

--- src/components/SearchBar/SearchBar.jsx
+++ src/components/SearchBar/SearchBar.jsx
@@ -1,9 +1,12 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { BsSearch } from 'react-icons/bs';
 
 import './SearchBar.css';
 
 function SearchBar() {
+
+  const [searchValue, setSearchValue] = useState('');
+
   return (
     <form className="search-bar">
       <input
@@ -11,8 +14,9 @@ function SearchBar() {
         placeholder="Buscar produtos"
         className="search_input"
         required
+        onChange={({ target }) => setSearchValue(target.value)}
       />
-
+      {searchValue}
       <button type="submit" className="search_button">
         <BsSearch />
       </button>
