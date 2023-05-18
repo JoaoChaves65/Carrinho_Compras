7a621e14837e84fc8af2df2de48d5d12ffdb48f1
JoaoChaves65 <joaomarcosgol2000@gmail.com>
2023-05-18 16:02:08 -0300

    Exibindo os produtos no card

--- src/components/ProductCard/ProductCard.jsx
+++ src/components/ProductCard/ProductCard.jsx
@@ -1,17 +1,25 @@
 import React from 'react';
 import { GrCart } from 'react-icons/gr';
+import propTypes from 'prop-types';
 
 import './ProductCard.css';
+import formatCurrency from '../../utils/formatCurrency';
 
-function ProductCard() {
+function ProductCard({ data }) {
+  const { title, price, thumbnail } = data;
+
   return (
     <section className="product-card">
 
-      <img src="https://http2.mlstatic.com/D_910401-MLA46545868460_062021-W.jpg" alt="product" className="card_image" />
+      <img
+        src={thumbnail.replace(/\w\.jpg/gi, 'W.jpg')}
+        alt="product"
+        className="card_image"
+      />
 
       <div className="card_infos">
-        <h2 className="card_price">R$ 200.20</h2>
-        <h2 className="card_title">Iphone</h2>
+        <h2 className="card_price">{formatCurrency(price, 'BRL')}</h2>
+        <h2 className="card_title">{title}</h2>
       </div>
 
       <button type="button" className="button_add-cart">
@@ -22,3 +30,7 @@ function ProductCard() {
 }
 
 export default ProductCard;
+
+ProductCard.propTypes = {
+  data: propTypes.shape({}),
+}.isrequired;
